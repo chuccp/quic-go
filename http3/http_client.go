2b2f01bb6650f88ae12a2365998330a5e7f73b77
tomcat <cooge123@gmail.com>
2023-08-14 14:15:31 +0800

    Update http_client.go

--- http3/http_client.go
+++ http3/http_client.go
@@ -1,13 +1,17 @@
 package http3
 
 import (
+	"crypto/tls"
 	"github.com/quic-go/quic-go"
 	"net"
 	"net/http"
 )
 
-func NewClient(conn net.PacketConn) *http.Client {
+func NewClient(conn net.PacketConn, config *tls.Config) *http.Client {
 	transport := &quic.Transport{Conn: conn}
-	roundTripper := &RoundTripper{transport: transport}
+	roundTripper := &RoundTripper{
+		transport:       transport,
+		TLSClientConfig: config,
+	}
 	return &http.Client{Transport: roundTripper}
 }
